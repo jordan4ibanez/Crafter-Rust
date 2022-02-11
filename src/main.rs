38f6fa667035538e765483456ce059bd12438fc1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-11 16:26:15 -0500

    Delete unused function

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,6 @@ extern crate glfw;
 use glfw::{ffi::*, Window, Context, WindowHint, OpenGlProfileHint};
 
 
-fn test () {
-
-}
-
 fn main() {
 
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
