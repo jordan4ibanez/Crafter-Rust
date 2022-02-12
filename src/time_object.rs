0ddef341dbfab8fb2669a52ced4fd8edb5632523
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-12 04:08:00 -0500

    Bolt on a delta time calculator to Time

--- src/time_object.rs
+++ src/time_object.rs
@@ -1,9 +1,14 @@
 extern crate glfw;
 
 pub struct Time {
+    // fps fields
     previous_time: f64,
     current_time: f64,
     frame_count: i32,
+
+    // delta fields
+    previous_delta: f64,
+    current_delta: f64
 }
 
 impl Time {
@@ -28,14 +33,30 @@ impl Time {
 
         (false, 0)
     }
+
+    #[inline(always)]
+    pub fn calculate_delta(&mut self, glfw: &glfw::Glfw) -> f64 {
+        self.current_delta = glfw.get_time();
+
+        let delta = self.current_delta - self.previous_delta;
+
+        self.previous_delta = glfw.get_time();
+
+        delta
+    }
 }
 
 #[inline(always)]
 pub fn new(glfw: &glfw::Glfw) -> Time {
     let current_time = glfw.get_time();
     Time {
-        previous_time: current_time.clone(),
-        current_time: current_time.clone(),
+        // timer fields
+        previous_time: current_time,
+        current_time,
         frame_count: 0,
+
+        // delta fields
+        previous_delta: current_time,
+        current_delta: current_time
     }
 }
