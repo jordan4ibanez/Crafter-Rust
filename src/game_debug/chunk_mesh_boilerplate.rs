fe05f61532007833b6af755b4ebcd55fe3bf5e5a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-15 16:12:20 -0500

    Deconstruct into more functional approach

--- src/game_debug/chunk_mesh_boilerplate.rs
+++ src/game_debug/chunk_mesh_boilerplate.rs
@@ -3,32 +3,18 @@
 // this is laid out in this manor because it is easier to debug
 
 
-// generic to reduce boilerplate
+
+// generic functions to reduce boilerplate
+
+// pushes the array slice into vector
 fn push<T: Copy>(vector: &mut Vec<T>, array: &[T]) {
     array.iter().for_each( | value: &T | {
         vector.push(*value);
     });
 }
 
-
-pub fn face_up(positions: &mut Vec<f32>, indices: &mut Vec<i32>, texture_coordinates: &mut Vec<f32>, colors: &mut Vec<f32>, x: f32, y: f32, z: f32) {
-
-
-
-    // vertex data
-
-    let mut pos: [f32; 18] = [
-
-        // tri 1
-        0.0, 1.0, 0.0,
-        0.0, 1.0, 1.0,
-        1.0, 1.0, 1.0,
-
-        // tri 2
-        1.0, 1.0, 0.0,
-        0.0, 1.0, 0.0,
-        1.0, 1.0, 1.0
-    ];
+// pushes the adjusted xyz into the vertex data
+fn set_pos(pos: &mut [f32], x: f32, y: f32, z: f32) {
 
     let mut xyz_index: i8 = 0;
 
@@ -47,15 +33,38 @@ pub fn face_up(positions: &mut Vec<f32>, indices: &mut Vec<i32>, texture_coordin
             xyz_index = 0;
         }
     });
+}
+
+// adjusts the indices to the correct value from base
+fn adjust_indices(index: &mut [i32], positions: &Vec<f32>) {
+    let adjustment: i32 = ( positions.len() as i32 - 18 ) / 3;
+    index.iter_mut().for_each( | value: &mut i32 | {
+        *value += adjustment;
+    });
+}
 
 
+pub fn face_up(positions: &mut Vec<f32>, indices: &mut Vec<i32>, texture_coordinates: &mut Vec<f32>, colors: &mut Vec<f32>, x: f32, y: f32, z: f32) {
+
+    
+    // vertex data
+
+    let mut pos: [f32; 18] = [
+
+        // tri 1
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 1.0,
+        1.0, 1.0, 1.0,
+
+        // tri 2
+        1.0, 1.0, 0.0,
+        0.0, 1.0, 0.0,
+        1.0, 1.0, 1.0
+    ];
+
+    set_pos(&mut pos, x, y, z);
 
     push(positions, &pos);
-
-
-
-
-
 
 
     // index (face/indices) data
@@ -68,25 +77,9 @@ pub fn face_up(positions: &mut Vec<f32>, indices: &mut Vec<i32>, texture_coordin
         3,4,5
     ];
 
-    // we must do simple calculations to get the correct indices
-    println!("LENGTH: {}", positions.len() / 3);
-
-    let indice_adjust: i32 = ( positions.len() as i32 - 18 ) / 3;
-
-    index.iter_mut().for_each( | value: &mut i32 | {
-        *value += indice_adjust;
-
-        println!("{},",value);
-    });
-
+    adjust_indices(&mut index, &positions);
+    
     push(indices, &index);
-
-
-
-
-
-
-
 
 
     // texture coordinates
@@ -108,11 +101,8 @@ pub fn face_up(positions: &mut Vec<f32>, indices: &mut Vec<i32>, texture_coordin
 
 
 
-
-
-
-
-    // light data
+    // light/color data
+    // TODO: intake as a parameter
     let color: [f32; 18] = [
 
         // tri 1
