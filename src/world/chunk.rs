5f961a35f98d4e2b103a6614512e0be051462fcf
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-16 13:04:06 -0500

    Write the basics of chunk

--- src/world/chunk.rs
+++ src/world/chunk.rs
@@ -1,10 +1,44 @@
-use glam::Vec2;
+use glam::{
+    IVec2
+};
+
+use crate::graphics::mesh::Mesh;
 
 
 pub struct Chunk {
-    position:  Vec2,
+    key: String,
+    position:  IVec2,
     block:     [u32; 32768],
     rotation:  [u8;  32768],
     light:     [u8;  32768],
-    heightmap: [u8;  256]
+    heightmap: [u8;  256],
+
+    mesh: Option<Mesh>
+}
+
+impl Chunk {
+    pub fn get_pos(&self) -> IVec2 {
+        self.position
+    }
+
+    pub fn get_key(&self) -> String {
+        self.key.clone()
+    }
+
+    pub fn get_mesh(&self) -> Option<&Mesh> {
+        self.mesh.as_ref()
+    }
+}
+
+
+pub fn new(x: i32, y: i32) -> Chunk {
+    Chunk {
+        key: x.to_string() + " " + &y.to_string(),
+        position: IVec2::new(x, y),
+        block: [0; 32768],
+        rotation: [0; 32768],
+        light: [0; 32768],
+        heightmap: [0; 256],
+        mesh: None
+    }
 }
