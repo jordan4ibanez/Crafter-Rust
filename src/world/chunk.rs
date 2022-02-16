fb090336d819c6b03631095ccddc4c77f5345127
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-16 14:02:41 -0500

    Add in important chunk methods

--- src/world/chunk.rs
+++ src/world/chunk.rs
@@ -28,6 +28,17 @@ impl Chunk {
     pub fn get_mesh(&self) -> Option<&Mesh> {
         self.mesh.as_ref()
     }
+
+    pub fn get_mesh_mut(&mut self) -> Option<&mut Mesh> {
+        self.mesh.as_mut()
+    }
+
+    pub fn set_mesh(&mut self, mesh: Mesh) {
+        match &self.mesh {
+            Some(existing_mesh) => existing_mesh.clean_up(false),
+            None => self.mesh = Some(mesh),
+        }
+    }
 }
 
 
@@ -42,3 +53,17 @@ pub fn new(x: i32, y: i32) -> Chunk {
         mesh: None
     }
 }
+
+/*
+pub fn new_with_mesh(x: i32, y: i32, mesh: Mesh) -> Chunk {
+    Chunk {
+        key: x.to_string() + " " + &y.to_string(),
+        position: IVec2::new(x, y),
+        block: [0; 32768],
+        rotation: [0; 32768],
+        light: [0; 32768],
+        heightmap: [0; 256],
+        mesh: Some(mesh)
+    }
+}
+*/
