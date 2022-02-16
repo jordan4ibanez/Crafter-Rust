56da08dd1d16fad7a29c86449bb5f722c4f88bca
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-16 14:01:56 -0500

    Increase sparsity for blocks

--- src/game_debug/chunk_mesh_creation.rs
+++ src/game_debug/chunk_mesh_creation.rs
@@ -56,7 +56,7 @@ pub fn create_chunk_mesh(texture: Texture, randy: &mut ThreadRng) -> Mesh {
 
     for i in 0..32768 {
 
-        debug_array[i] = randy.gen::<f32>() > 0.5;
+        debug_array[i] = randy.gen::<f32>() > 0.98;
 
         if debug_array[i] {
             for _ in 0..6 {
