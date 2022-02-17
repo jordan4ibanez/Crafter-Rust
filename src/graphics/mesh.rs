acc4e53f29fe377825331322323695b241dda35d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-17 14:04:46 -0500

    Add wireframe mode

--- src/graphics/mesh.rs
+++ src/graphics/mesh.rs
@@ -130,6 +130,9 @@ impl Mesh {
 
     pub fn batch_render(&self) {
         unsafe {
+            // this is for debug, wireframe mode
+            gl::PolygonMode(gl::FRONT_AND_BACK,gl::LINE);
+
             // bind the mesh vertex array
             gl::BindVertexArray(self.vao_id);
 
