b88ad3634a803b70a28016a2db24f65eee16bb8e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-17 22:04:52 -0500

    Create function to automatically get the system path as string

--- src/graphics/resource_loader.rs
+++ src/graphics/resource_loader.rs
@@ -6,15 +6,17 @@ use std::{
     io::Read
 };
 
-pub fn load_resource(path: String) -> String {
-    let system_path: String = std::env::current_dir()
+pub fn get_path_string() -> String {
+    std::env::current_dir()
         .unwrap()
         .as_os_str()
         .to_str()
         .unwrap()
-        .to_owned();
-    
-    let resource_result = fs::read_to_string(system_path + &path);    
+        .to_owned()
+}
+
+pub fn load_resource(path: String) -> String {    
+    let resource_result = fs::read_to_string(get_path_string() + &path);    
 
     match resource_result {
         Ok(data) => data,
@@ -22,9 +24,9 @@ pub fn load_resource(path: String) -> String {
     }    
 }
 
-pub fn load_texture(path: String) -> Vec<u8>{
+pub fn load_texture(path: &str) -> Vec<u8>{
 
-    let mut file: File = File::open(path).expect("COULD NOT LOAD IMAGE!");
+    let mut file: File = File::open(get_path_string() + path).expect("COULD NOT LOAD IMAGE!");
     let mut data: Vec<u8> = Vec::<u8>::new();
     file.read_to_end(&mut data).expect("COULD NOT PARSE IMAGE DATA!");
 
