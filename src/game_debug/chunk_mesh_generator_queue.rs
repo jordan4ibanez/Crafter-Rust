bb913ef604ed6f6112952de2c8911f48d7956b88
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-17 19:45:31 -0500

    Implement basic queue

--- src/game_debug/chunk_mesh_generator_queue.rs
+++ src/game_debug/chunk_mesh_generator_queue.rs
@@ -5,6 +5,34 @@ struct MeshUpdate {
     update_neighbors: bool
 }
 
-struct ChunkMeshGenerator {
+impl MeshUpdate {
+    pub fn get_key(&self) -> &String {
+        &self.key
+    }
+    pub fn update_neighbors(&self) -> bool {
+        self.update_neighbors
+    }
+}
+
+struct ChunkMeshGeneratorQueue {
     queue: Vec<MeshUpdate>
+}
+
+// this is a lot like Deque in Java - at least the parts that I use
+impl ChunkMeshGeneratorQueue {
+    pub fn new() -> Self {
+        ChunkMeshGeneratorQueue {
+            queue: Vec::new(),
+        }
+    }
+    pub fn put(&mut self, x: i32, z: i32, update_neighbors: bool){
+        let update: MeshUpdate = MeshUpdate {
+            key: x.to_string() + " " + &z.to_string(),
+            update_neighbors,
+        };
+        self.queue.push(update);
+    }
+    pub fn pop(&mut self) -> Option<MeshUpdate> {
+        self.queue.pop()
+    }
 }
