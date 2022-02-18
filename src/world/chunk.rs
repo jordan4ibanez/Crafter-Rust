fbe17ac120306f628b5afa2359ed7605fe11fb87
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-17 19:22:49 -0500

    Fix typo, precalculation internal

--- src/world/chunk.rs
+++ src/world/chunk.rs
@@ -33,11 +33,15 @@ impl Chunk {
         self.position
     }
 
-    pub fn get_block_aray_mut(&mut self) -> &mut [u32; 32768] {
+    pub fn get_block(&self, x: i8, y: i8, z: i8) -> u32 {
+        self.block[mini_pos_to_index(x, y, z) as usize]
+    }
+
+    pub fn get_block_array_mut(&mut self) -> &mut [u32; 32768] {
         &mut self.block
     }
 
-    pub fn get_block_aray(&self) -> &[u32; 32768] {
+    pub fn get_block_array(&self) -> &[u32; 32768] {
         &self.block
     }
 
@@ -64,16 +68,19 @@ impl Chunk {
     }
 }
 
-/*
-pub fn new_with_mesh(x: i32, y: i32, mesh: Mesh) -> Chunk {
-    Chunk {
-        key: x.to_string() + " " + &y.to_string(),
-        position: IVec2::new(x, y),
-        block: [0; 32768],
-        rotation: [0; 32768],
-        light: [0; 32768],
-        heightmap: [0; 256],
-        mesh: Some(mesh)
-    }
+fn mini_index_to_pos(i: u16) -> (i8,i8,i8) {
+    let mut index :u16 = i.clone();
+    let x: i8 = (index / 2048) as i8;
+    index = index % 2048;
+    let z: i8 = (index / 128) as i8;
+    index = index % 128;
+    let y: i8 = index as i8;
+    (x, y, z)
 }
-*/
+
+pub fn mini_pos_to_index ( x: i8, y: i8, z: i8 ) -> u16 {
+    let x_wide: u16 = x as u16;
+    let y_wide: u16 = y as u16;
+    let z_wide: u16 = z as u16;
+    (x_wide * 2048) + (z_wide * 128) + y_wide
+}
