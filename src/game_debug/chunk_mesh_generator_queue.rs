538ffa1900e8690acc8310eb60de76a9b2a45591
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-17 20:01:55 -0500

    Implement batching & specific queue functions

--- src/game_debug/chunk_mesh_generator_queue.rs
+++ src/game_debug/chunk_mesh_generator_queue.rs
@@ -1,20 +1,24 @@
 
 // mesh updates hold data on which chunks should be updated
-struct MeshUpdate {
-    key: String,
+pub struct MeshUpdate {
+    x: i32,
+    z: i32,
     update_neighbors: bool
 }
 
 impl MeshUpdate {
-    pub fn get_key(&self) -> &String {
-        &self.key
+    pub fn get_x(&self) -> i32 {
+        self.x
+    }
+    pub fn get_z(&self) -> i32 {
+        self.z
     }
     pub fn update_neighbors(&self) -> bool {
         self.update_neighbors
     }
 }
 
-struct ChunkMeshGeneratorQueue {
+pub struct ChunkMeshGeneratorQueue {
     queue: Vec<MeshUpdate>
 }
 
@@ -25,9 +29,38 @@ impl ChunkMeshGeneratorQueue {
             queue: Vec::new(),
         }
     }
+    // stores an update for all neighbors - does not recursively generate more
+    pub fn batch_neighbor_update(&mut self, x: i32, z: i32){
+        let update_1: MeshUpdate = MeshUpdate {
+            x: x + 1,
+            z,
+            update_neighbors: false
+        };
+        let update_2: MeshUpdate = MeshUpdate {
+            x: x - 1,
+            z,
+            update_neighbors: false
+        };
+        let update_3: MeshUpdate = MeshUpdate {
+            x,
+            z: z + 1,
+            update_neighbors: false
+        };
+        let update_4: MeshUpdate = MeshUpdate {
+            x,
+            z: z - 1,
+            update_neighbors: false
+        };
+
+        self.queue.push(update_1);
+        self.queue.push(update_2);
+        self.queue.push(update_3);
+        self.queue.push(update_4);
+    }
     pub fn put(&mut self, x: i32, z: i32, update_neighbors: bool){
         let update: MeshUpdate = MeshUpdate {
-            key: x.to_string() + " " + &z.to_string(),
+            x,
+            z,
             update_neighbors,
         };
         self.queue.push(update);
