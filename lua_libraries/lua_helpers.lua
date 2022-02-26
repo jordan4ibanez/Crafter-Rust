a0c74130c26bb3d09770c63d045226453372ccad
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-25 20:39:07 -0500

    Lua: Add table dump

--- lua_libraries/lua_helpers.lua
+++ lua_libraries/lua_helpers.lua
@@ -1,6 +1,21 @@
 -- this value is passed in from Rust
 local operating_system = operating_system
 
+-- A simple getter for current operating system.
 function get_operating_system()
     return operating_system
 end
+
+-- A simple table dump
+function dump(o)
+    if type(o) == 'table' then
+       local s = '{ '
+       for k,v in pairs(o) do
+          if type(k) ~= 'number' then k = '"'..k..'"' end
+          s = s .. '['..k..'] = ' .. dump(v) .. ','
+       end
+       return s .. '} '
+    else
+       return tostring(o)
+    end
+ end
