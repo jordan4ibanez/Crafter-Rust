8daa06195a6d69bc8d1b3aca3365e22f90c051fe
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-25 21:35:30 -0500

    Create main.lua

---
+++ mods/bed/main.lua
@@ -0,0 +1,17 @@
+crafter.register_block({
+    name = "blah",
+    textures = {"blah.png"}
+})
+
+crafter.register_block({
+    name = "blah1",
+    textures = {"blah1.png"}
+})
+crafter.register_block({
+    name = "blah2",
+    textures = {"blah2.png"}
+})
+crafter.register_block({
+    name = "blah3",
+    textures = {"blah3.png"}
+})
