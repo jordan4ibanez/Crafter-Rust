5d659ce9b63072bbaf6a22ac903bd5ffdd18767d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-25 21:10:16 -0500

    Update lua_initialize.rs

--- src/lua/lua_initialize.rs
+++ src/lua/lua_initialize.rs
@@ -4,11 +4,14 @@ use mlua::Lua;
 
 use super::lua_functions::load_lua_file;
 
-pub fn initialize_lua() {
+pub fn initialize_lua() -> Lua {
     let lua: Lua = Lua::new();
     
     // tells lua which operating system is being used
     lua.globals().set("operating_system", env::consts::OS).unwrap();
 
+    // loads the lua context (entry point)
     lua.load(&load_lua_file("/lua_libraries/lua_context.lua")).exec().unwrap();
+
+    lua
 }
