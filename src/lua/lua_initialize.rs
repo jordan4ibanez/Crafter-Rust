cdb5cf4bd5c82670be3725dede968966615be1dc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-25 19:47:58 -0500

    Tell lua which os is being used

--- src/lua/lua_initialize.rs
+++ src/lua/lua_initialize.rs
@@ -1,9 +1,14 @@
+use std::env;
+
 use mlua::Lua;
 
 use super::lua_functions::load_lua_file;
 
 pub fn initialize_lua() {
     let lua: Lua = Lua::new();
+    
+    // tells lua which operating system is being used
+    lua.globals().set("operating_system", env::consts::OS).unwrap();
 
     lua.load(&load_lua_file("/lua_libraries/lua_context.lua")).exec().unwrap();
 }
