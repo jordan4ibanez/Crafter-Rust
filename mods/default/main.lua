7d9078d8426cbd1934017b352983b22f4e16b2ac
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-25 20:03:57 -0500

    Create first game mod

---
+++ mods/default/main.lua
@@ -0,0 +1,11 @@
+crafter.register_block({
+    name = "dirt",
+    textures = {
+        "dirt.png",
+        "dirt.png",
+        "dirt.png",
+        "dirt.png",
+        "dirt.png",
+        "dirt.png"
+    }
+})
