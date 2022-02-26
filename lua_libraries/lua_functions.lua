0fad59d41316a317faf3dc36198aa01c39fac439
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-25 20:39:20 -0500

    Lua: Check against all existing textures

--- lua_libraries/lua_functions.lua
+++ lua_libraries/lua_functions.lua
@@ -1,5 +1,8 @@
 -- Lua Locals.
-local insert = table.insert
+local 
+insert      , dump
+=
+table.insert, dump
 
 local function cache_texture_to_load(mod, texture_table)
 
@@ -8,28 +11,31 @@ local function cache_texture_to_load(mod, texture_table)
         return
     end
 
+    -- Cache table pointer.
+    local cache = crafter.texture_cache
+
     -- Create a texture cache table if non-existent.
-    if crafter.texture_cache[mod] == nil then
-        crafter.texture_cache[mod] = {}
+    if cache[mod] == nil then
+        cache[mod] = {}
     end
 
-    -- Cache table pointer.
-    local cache = crafter.texture_cache[mod]
 
     -- Run through textures and see if they need to be cached.
     for _,current_texture in ipairs(texture_table) do
 
         local found = false
 
-        -- Check against existing values.
-        for _,cached_texture in ipairs(cache) do
-            if current_texture == cached_texture then
-                found = true
+        -- Check against existing textures in all mods.
+        for _,mod_table in pairs(cache) do
+            for _,cached_texture in ipairs(mod_table) do
+                if current_texture == cached_texture then
+                    found = true
+                end
             end
         end
 
         if not found then
-            insert(cache, current_texture)
+            insert(cache[mod], current_texture)
         end
     end
 end
@@ -40,6 +46,7 @@ crafter.register_block = function(table_data)
     local mod = current_loading_mod
     table_data.mod = mod
 
+    -- Create streamlined texture cache for Rust to work with.
     cache_texture_to_load(mod, table_data.textures)
 
     crafter.blocks[table_data.name] = table_data
