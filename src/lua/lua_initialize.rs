a0804158d52b9417b3d70b53cfc500ad9b3230bb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-25 19:45:18 -0500

    Run lua api context creation

--- src/lua/lua_initialize.rs
+++ src/lua/lua_initialize.rs
@@ -1,7 +1,9 @@
 use mlua::Lua;
 
+use super::lua_functions::load_lua_file;
+
 pub fn initialize_lua() {
     let lua: Lua = Lua::new();
 
-    
+    lua.load(&load_lua_file("/lua_libraries/lua_context.lua")).exec().unwrap();
 }
