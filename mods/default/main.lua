d61e6ada7eaaa45921b96d76f5d9b2591e0a5a78
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-25 21:05:51 -0500

    Update main.lua

--- mods/default/main.lua
+++ mods/default/main.lua
@@ -2,22 +2,6 @@ crafter.register_block({
     name = "dirt",
     textures = {
         "dirt.png",
-        "dirt.png",
-        "dirt.png",
-        "dirt.png",
-        "dirt.png",
-        "dirt.png"
-    }
-})
-
-crafter.register_block({
-    name = "friday",
-    textures = {
-        "dirt.png",
-        "dirt.png",
-        "dirt.png",
-        "dirt.png",
-        "dirt.png",
-        "dirt.png"
+        "stone.png"
     }
 })
