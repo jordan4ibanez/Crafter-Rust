1cb8521ee8dd8f70df80d48a9c42675b5c81f9d5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-26 19:38:09 -0500

    Make current_working_directory less jank in Lua

--- src/lua/lua_initialize.rs
+++ src/lua/lua_initialize.rs
@@ -2,6 +2,8 @@ use std::env;
 
 use mlua::{Lua, Table};
 
+use crate::helper::helper_functions::get_path_string;
+
 use super::lua_functions::load_lua_file;
 
 pub fn initialize_lua() -> Lua {
@@ -9,6 +11,9 @@ pub fn initialize_lua() -> Lua {
     
     // tells lua which operating system is being used
     lua.globals().set("operating_system", env::consts::OS).unwrap();
+
+    // tells lua where the root of the folder is
+    lua.globals().set("current_working_directory", get_path_string()).unwrap();
 
     // loads the lua context (entry point)
     lua.load(&load_lua_file("/lua_libraries/lua_context.lua")).exec().unwrap();
