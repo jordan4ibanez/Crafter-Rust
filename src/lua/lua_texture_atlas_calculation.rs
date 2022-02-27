4a8da6352a5fb782bd58c794df9bbd1906c7edbc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-26 22:49:04 -0500

    Create normal draw type texture pre-calculation

--- src/lua/lua_texture_atlas_calculation.rs
+++ src/lua/lua_texture_atlas_calculation.rs
@@ -1,7 +1,34 @@
+use texture_packer::{Frame, Rect};
+
 use crate::blocks::blocks::AtlasTextureMap;
 
-pub fn calculate_atlas_location(
+pub fn calculate_atlas_location_normal(
+    atlas_width: u32,
+    atlas_height: u32,
+    frame: &Frame<String>
+) -> AtlasTextureMap {
+    
+    println!("{:#?}", frame);
 
-) /*-> AtlasTextureMap*/ {
+    let rectangle_frame: Rect = frame.frame;
 
+    // convert all to f32 so the calculations are readable
+    let atlas_width_f32: f32 = atlas_width as f32;
+    let atlas_height_f32: f32 = atlas_height as f32;
+
+    // base location (top left) x
+    let x_f32: f32 = rectangle_frame.x as f32;
+    // base location (top left) y
+    let y_f32: f32 = rectangle_frame.y as f32;
+    // width of texture
+    let w_f32: f32 = rectangle_frame.w as f32;
+    // height of texture
+    let h_f32: f32 = rectangle_frame.h as f32;
+
+    AtlasTextureMap::new(
+        atlas_width_f32 / x_f32, 
+        atlas_height_f32 / y_f32, 
+        atlas_width_f32 / (x_f32 + w_f32), 
+        atlas_width_f32 / (y_f32 + h_f32)
+    )
 }
