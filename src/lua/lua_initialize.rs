6c8713bd830f73765735dcfd6339f7e03bda39e3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-27 22:06:04 -0500

    Add better Lua debugging :8ball:

--- src/lua/lua_initialize.rs
+++ src/lua/lua_initialize.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use mlua::{Lua, Table};
+use mlua::{Lua, Error};
 
 use crate::helper::helper_functions::get_path_string;
 
@@ -15,8 +15,16 @@ pub fn initialize_lua() -> Lua {
     // tells lua where the root of the folder is
     lua.globals().set("current_working_directory", get_path_string()).unwrap();
 
-    // loads the lua context (entry point)
-    lua.load(&load_lua_file("/lua_libraries/lua_context.lua")).exec().unwrap();
+    /*
+    loads the lua context (entry point)
+    this is an mlua::Error enum
+    */
+    let lua_error: Result<(), Error> = lua.load(&load_lua_file("/lua_libraries/lua_context.lua")).exec();
+
+    match lua_error {
+        Ok(_) => (),
+        Err(error) => panic!("{}", error.to_string())
+    }
 
     lua
 }
