5ff069d27a9735d90636ecea1c892fd4a00c9d5d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-03-01 05:18:00 -0500

    Flesh out gcs

--- src/biomes/generation_component_system.rs
+++ src/biomes/generation_component_system.rs
@@ -5,7 +5,7 @@ pub struct LayerDepth {
 
 impl LayerDepth {
     pub fn new(min: u8, max: u8) -> Self {
-        LayerDepth {
+        Self {
             min,
             max
         }
@@ -15,13 +15,76 @@ impl LayerDepth {
     }
 }
 
+pub struct NoiseParams {
+    min: f32,
+    max: f32
+}
+
+impl NoiseParams {
+    pub fn new(min: f32, max: f32) -> Self {
+        Self {
+            min,
+            max,
+        }
+    }
+    pub fn get(&self) -> (f32, f32) {
+        (self.min, self.max)
+    }
+}
+
+
+
 pub struct GenerationComponentSystem {
+
     id: Vec<String>,
+
     name: Vec<String>,
-    top_layer: Vec<u32>
+
+    top_layer: Vec<u32>,
+    top_layer_depth: Vec<LayerDepth>,
+
+    bottom_layer: Vec<u32>,
+    bottom_layer_depth: Vec<LayerDepth>,
+
+    stone_layer: Vec<u32>,
+
+    // how high or low the terrain can fluctuate
+    terrain_noise_multiplier: Vec<u8>,
+
+    // how often the terrain fluctuates
+    terrain_frequency: Vec<f32>,
+
+    // defines if there is cave generation
+    caves: Vec<bool>,
+
+    // minimum and maximum noise for a cave to be carved
+    cave_heat: Vec<NoiseParams>,
+
+    // defines if there is rain
+    rain: Vec<bool>,
+
+    // defines if there is snow
+    snow: Vec<bool>
 
 }
 
 impl GenerationComponentSystem {
+    pub fn new() -> Self {
+        Self {
+            id: Vec::new(),
+            name: Vec::new(),
+            top_layer: Vec::new(),
+            top_layer_depth: Vec::new(),
+            bottom_layer: Vec::new(),
+            bottom_layer_depth: Vec::new(),
+            stone_layer: Vec::new(),
+            terrain_noise_multiplier: Vec::new(),
+            terrain_frequency: Vec::new(),
+            caves: Vec::new(),
+            cave_heat: Vec::new(),
+            rain: Vec::new(),
+            snow: Vec::new(),
+        }
+    }
     
 }
