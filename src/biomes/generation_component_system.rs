78bd4eb3af0e96bf4d3f4c4a3939591a6d10fabe
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-03-01 05:03:47 -0500

    Add more structure to gcs

--- src/biomes/generation_component_system.rs
+++ src/biomes/generation_component_system.rs
@@ -1,5 +1,24 @@
+pub struct LayerDepth {
+    min: u8,
+    max: u8
+}
+
+impl LayerDepth {
+    pub fn new(min: u8, max: u8) -> Self {
+        LayerDepth {
+            min,
+            max
+        }
+    }
+    pub fn get(&self) -> (u8, u8) {
+        (self.min, self.max)
+    }
+}
+
 pub struct GenerationComponentSystem {
-    
+    id: Vec<String>,
+    name: Vec<String>,
+    top_layer: Vec<u32>
 
 }
 
