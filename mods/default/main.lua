d3520372a08929d846760b5f8e96b873a35b793a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-03-01 22:13:42 -0500

    Lua: Trim biome cave gen

--- mods/default/main.lua
+++ mods/default/main.lua
@@ -60,10 +60,10 @@ crafter.register_biome({
     caves = true,
 
     -- Minimum and maximum noise for cave to be carved.
-    cave_heat = {0.8, 1.0},
+    cave_heat = {0.7, 1.0},
 
     -- How often cave carving fluctuates.
-    cave_frequency = 0.01,
+    cave_frequency = 0.02,
 
     -- Defines if there is rain.
     rain = true,
