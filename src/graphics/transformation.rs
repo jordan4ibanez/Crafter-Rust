d0c038a1303975d65c3b5936484170f342856c5b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-03-04 04:15:57 -0500

    Optimize rotation transformation

--- src/graphics/transformation.rs
+++ src/graphics/transformation.rs
@@ -10,6 +10,10 @@ pub struct Transformation {
     projection_matrix: Mat4,
     model_matrix: Mat4,
     view_matrix: Mat4, // <- this is an identity - reference Mat4 TODO: rename this to camera_matrix
+    rotation_x: Vec3,
+    rotation_y: Vec3,
+    rotation_z: Vec3,
+    camera_position: Vec3
 }
 
 impl Transformation {
@@ -18,7 +22,11 @@ impl Transformation {
         Self {
             projection_matrix: Mat4::IDENTITY,
             model_matrix: Mat4::IDENTITY,
-            view_matrix: Mat4::IDENTITY
+            view_matrix: Mat4::IDENTITY,
+            rotation_x: Vec3::new(1.0,0.0,0.0),
+            rotation_y: Vec3::new(0.0,1.0,0.0),
+            rotation_z: Vec3::new(0.0,0.0,1.0),
+            camera_position: Vec3::splat(0.0)
         }
     }
 
@@ -42,9 +50,11 @@ impl Transformation {
         self.view_matrix *= Mat4::from_axis_angle(Vec3::new(0.0,1.0,0.0), camera.get_rot().y.to_radians());
 
         // this is also inverted
-        let my_vector: Vec3 = Vec3::new(-camera.get_pos().x, -camera.get_pos().y, -camera.get_pos().z);
+        self.camera_position.x = -camera.get_pos_x();
+        self.camera_position.y = -camera.get_pos_y();
+        self.camera_position.z = -camera.get_pos_z();
 
-        self.view_matrix *= Mat4::from_translation(my_vector);
+        self.view_matrix *= Mat4::from_translation(self.camera_position);
 
     }
 
@@ -60,9 +70,9 @@ impl Transformation {
 
         // rotation
 
-        self.model_matrix *= Mat4::from_axis_angle(Vec3::new(1.0,0.0,0.0), rot.x);
-        self.model_matrix *= Mat4::from_axis_angle(Vec3::new(0.0,1.0,0.0), rot.y);
-        self.model_matrix *= Mat4::from_axis_angle(Vec3::new(0.0,0.0,1.0), rot.z);
+        self.model_matrix *= Mat4::from_axis_angle(self.rotation_x, rot.x);
+        self.model_matrix *= Mat4::from_axis_angle(self.rotation_y, rot.y);
+        self.model_matrix *= Mat4::from_axis_angle(self.rotation_z, rot.z);
 
         self.model_matrix
     }
